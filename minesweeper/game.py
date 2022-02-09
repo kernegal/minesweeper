c4ccb823b2bf04c67baaf4c11bbec274f21fb291
Legan Nomert <kernegal@users.noreply.github.com>
2022-02-10 00:06:39 +0100

    Initial barebones

---
+++ minesweeper/game.py
@@ -0,0 +1,15 @@
+class Game:
+    def __init__(self):
+        pass
+
+    def draw_grid(self):
+        pass
+
+    def main_loop(self):
+        pass
+
+    def check_events(self):
+        pass
+
+    def run(self):
+        pass
---
+++ minesweeper/gameboard.py
@@ -0,0 +1,12 @@
+class GameBoard:
+    def __init__(self, size_x: int, size_y: int, num_bombs: int):
+        pass
+
+    def is_bomb(self, x: int, y: int) -> bool:
+        pass
+
+    def count_bombs(self, x: int, y: int) -> bool:
+        pass
+
+    def check_tile(self, x: int, y: int) -> None:
+        pass
