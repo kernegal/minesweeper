135888956ce02800425dc3e92d5fe576b3289fb0
Legan Nomert <kernegal@users.noreply.github.com>
2022-02-10 12:45:29 +0100

    Create the problems and remove the code that needs to be completed

--- minesweeper/gameboard.py
+++ minesweeper/gameboard.py
@@ -12,23 +12,23 @@ class GameBoard:
     def __init__(self, size_x: int, size_y: int, num_bombs: int):
         self.size_x = size_x
         self.size_y = size_y
-        self.grid = [list(GameBoard.EMPTY for _ in range(size_x)) for _ in range(size_y)]
-        self.clear = [list(GameBoard.HIDDEN for _ in range(size_x)) for _ in range(size_y)]
 
-        bombs_options = [(x, y) for x in range(size_x) for y in range(size_y)]
-        for x, y in random.sample(bombs_options, num_bombs):
-            self.grid[y][x] = GameBoard.BOMB
+        # PROBLEM 1: Create 2 matrix (an array of arrays) with size_x columns and size_y lines. The initial values
+        # should be self.grid should GameBoard.EMPTY for self.grid and GameBoard.HIDDEN for self.clear
+        self.grid = [[GameBoard.EMPTY]]
+        self.clear = [[GameBoard.HIDDEN]]
+
+        # PROBLEM 5: Place GameBoard.BOMB in the self.grid. Take care of no placing two bombs int the same cell
+        # Tip: You can use random.randint(min,max) to get a random number or random.sample to get a sample from a list
+        self.grid[0][0] = GameBoard.BOMB
 
     def is_bomb(self, x: int, y: int) -> bool:
-        return self.grid[y][x] == GameBoard.BOMB
+        # Problem 3: Check if a cell is a bomb. Return True if it is and False if not
+        return False
 
     def count_bombs(self, x: int, y: int) -> int:
+        # Problem 4: Count the number of bombs around the cell x, y and return the value
         bombs = 0
-        for a in range(x-1, x+2):
-            for b in range(y-1, y+2):
-                if self.is_valid(a, b):
-                    bombs += self.is_bomb(a, b)
-
         return bombs
 
     def is_checked(self, x: int, y: int) -> bool:
@@ -44,7 +44,8 @@ class GameBoard:
         return self.clear[y][x] == GameBoard.MARKED
 
     def is_valid(self, x: int, y: int) -> bool:
-        return 0 <= x < self.size_x and 0 <= y < self.size_y
+        # Problem 2: Return True if x and y corresponds to a cell in the grid and False otherwise
+        return True
 
     def check_tile(self, x: int, y: int) -> bool:
         tiles_to_be_checked = [(x, y)]
@@ -57,17 +58,16 @@ class GameBoard:
             self.clear[tile[1]][tile[0]] = GameBoard.CLEAR
 
             if bombs == 0:
-                next_to_check = [(a, b)
-                                 for a in range(tile[0]-1, tile[0]+2) for b in range(tile[1]-1, tile[1]+2)
-                                 if self.is_valid(a, b) and not self.is_checked(a, b) ]
+                # Problem 7: Create a list of tuples containing the x and y coordinates of all cells around the actual
+                # cell. It shouldn't contain the actual cell, nor cells outside the grid, nor the cells that are
+                # actually checked
+                next_to_check = []
 
                 tiles_to_be_checked += next_to_check
 
         return False
 
     def mark_tile(self, x: int, y: int) -> None:
-        if self.is_valid(x, y) and not self.is_checked(x, y):
-            if self.is_marked(x, y):
-                self.clear[y][x] = GameBoard.HIDDEN
-            else:
-                self.clear[y][x] = GameBoard.MARKED
+        # Problem 6: Set the cell x, y in self.clear as GameBoard.MARKED if it is not checked (you can use
+        # self.is_checked(x,y)). If it is already marked, return it to GameBoard.HIDDEN
+        self.clear[0][0] = GameBoard.MARKED
