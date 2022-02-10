ed6606e858dc9d893301cafe4534b20a0ba67d94
Legan Nomert <kernegal@users.noreply.github.com>
2022-02-10 03:02:00 +0100

    Work done almost

--- minesweeper/game.py
+++ minesweeper/game.py
@@ -1,15 +1,87 @@
+import pygame, sys
+from pygame.locals import *
+
+from minesweeper.gameboard import GameBoard
+
+
 class Game:
+    BLACK = (0, 0, 0)
+    WHITE = (200, 200, 200)
+    BLUE = (100, 120, 200)
+    RED = (200, 50, 50)
+
     def __init__(self):
-        pass
+        self.x_tiles = 10
+        self.y_tiles = 10
+
+        self.game_board = GameBoard(self.x_tiles, self.y_tiles, 5)
+        self.tile_size = 20
+
+        self.window_height = self.y_tiles * self.tile_size
+        self.window_width = self.x_tiles * self.tile_size
+
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((self.window_width, self.window_height))
+        self.clock = pygame.time.Clock()
+
+        self.screen.fill(Game.BLACK)
+
+        font = pygame.freetype.SysFont(["DejaVu Sans", "Arial", "Helvetica"], self.tile_size - 2)
+        self.numbers = [font.render(str(i), Game.WHITE) for i in range(8)]
+
+        self.dead = False
 
     def draw_grid(self):
-        pass
+        for x in range(self.x_tiles):
+            for y in range(self.y_tiles):
+                if not self.game_board.is_checked(x, y):
+                    pygame.draw.rect(self.screen,
+                                     Game.WHITE,
+                                     [self.tile_size * x + 1,
+                                      self.tile_size * y + 1,
+                                      self.tile_size - 2,
+                                      self.tile_size - 2])
+                    if self.game_board.is_marked(x, y):
+                        pygame.draw.circle(self.screen,
+                                           Game.BLUE,
+                                           (x * self.tile_size + self.tile_size / 2,
+                                            y * self.tile_size + self.tile_size / 2),
+                                           (self.tile_size-2) / 2)
+                else:
+                    num = self.game_board.count_bombs(x, y)
+                    if num != 0:
+                        self.screen.blit(self.numbers[num][0], (x * self.tile_size+2, y * self.tile_size+2))
+
+                if self.dead and self.game_board.is_bomb(x, y):
+                    pygame.draw.circle(self.screen,
+                                       Game.RED,
+                                       (x * self.tile_size + self.tile_size / 2,
+                                        y * self.tile_size + self.tile_size / 2),
+                                       (self.tile_size-2) / 2)
 
     def main_loop(self):
-        pass
+        self.screen.fill(Game.BLACK)
+        self.draw_grid()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-    def check_events(self):
-        pass
+            if event.type == pygame.MOUSEBUTTONUP and not self.dead:
+                pos = event.pos
+                mouse_pos = pygame.mouse.get_pos()
+                mouse_pos_grid_x = mouse_pos[0] // self.tile_size
+                mouse_pos_grid_y = mouse_pos[1] // self.tile_size
+                if event.button == 1:  # left click
+                    self.dead = self.game_board.check_tile(mouse_pos_grid_x, mouse_pos_grid_y)
+
+                if event.button == 3:  # right click
+                    self.game_board.mark_tile(mouse_pos_grid_x, mouse_pos_grid_y)
+
+        self.clock.tick(60)
+        pygame.display.flip()
 
     def run(self):
-        pass
+        while True:
+            self.main_loop()
--- minesweeper/gameboard.py
+++ minesweeper/gameboard.py
@@ -1,12 +1,75 @@
+import random
+
+
 class GameBoard:
+    EMPTY = 0
+    BOMB = 1
+
+    HIDDEN = 0
+    CLEAR = 1
+    MARKED = 2
+
     def __init__(self, size_x: int, size_y: int, num_bombs: int):
-        pass
+        self.size_x = size_x
+        self.size_y = size_y
+        self.grid = [list(GameBoard.EMPTY for _ in range(size_x)) for _ in range(size_y)]
+        self.clear = [list(GameBoard.HIDDEN for _ in range(size_x)) for _ in range(size_y)]
+        self.near_bombs = [list(0 for _ in range(size_x)) for _ in range(size_y)]
+
+        bombs_options = [(x, y) for x in range(size_x) for y in range(size_y)]
+        for x, y in random.sample(bombs_options, num_bombs):
+            self.grid[y][x] = GameBoard.BOMB
 
     def is_bomb(self, x: int, y: int) -> bool:
-        pass
+        return self.grid[y][x] == GameBoard.BOMB
 
-    def count_bombs(self, x: int, y: int) -> bool:
-        pass
+    def count_bombs(self, x: int, y: int) -> int:
+        bombs = 0
+        for a in range(x-1, x+2):
+            for b in range(y-1, y+2):
+                if self.is_valid(a, b):
+                    bombs += self.is_bomb(a, b)
 
-    def check_tile(self, x: int, y: int) -> None:
-        pass
+        return bombs
+
+    def is_checked(self, x: int, y: int) -> bool:
+        """
+        Check if a tile is already visited
+        :param x: x position
+        :param y: y position
+        :return: True if visited
+        """
+        return self.clear[y][x] == GameBoard.CLEAR
+
+    def is_marked(self, x: int, y: int) -> bool:
+        return self.clear[y][x] == GameBoard.MARKED
+
+    def is_valid(self, x: int, y: int) -> bool:
+        return 0 <= x < self.size_x and 0 <= y < self.size_y
+
+    def check_tile(self, x: int, y: int) -> bool:
+        tiles_to_be_checked = [(x, y)]
+        while tiles_to_be_checked:
+            tile = tiles_to_be_checked.pop()
+            if self.is_bomb(*tile):
+                return True
+
+            bombs = self.count_bombs(*tile)
+            self.clear[tile[1]][tile[0]] = GameBoard.CLEAR
+            self.near_bombs[tile[1]][tile[0]] = bombs
+
+            if bombs == 0:
+                next_to_check = [(a, b)
+                                 for a in range(tile[0]-1, tile[0]+2) for b in range(tile[1]-1, tile[1]+2)
+                                 if self.is_valid(a, b) and not self.is_checked(a, b) ]
+
+                tiles_to_be_checked += next_to_check
+
+        return False
+
+    def mark_tile(self, x: int, y: int) -> None:
+        if self.is_valid(x, y) and not self.is_checked(x, y):
+            if self.is_marked(x, y):
+                self.clear[y][x] = GameBoard.HIDDEN
+            else:
+                self.clear[y][x] = GameBoard.MARKED
